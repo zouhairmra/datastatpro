e0e6d8260dc0ba23bfeb825afa4dc432584c210d
zouhairmra <zouhairmrabet@gmail.com>
2025-07-17 23:18:21 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+
+import streamlit as st
+
+st.set_page_config(page_title="DataStatPro Clone", layout="wide")
+
+st.image("logo.png", width=150)
+st.title("Welcome to DataStatPro Clone")
+
+st.markdown("""
+This is the homepage of your interactive economic & financial analytics app.
+Use the sidebar to navigate through different analysis tools.
+""")
