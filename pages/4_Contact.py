154e257fa9960660927299f188f1354aa7e1970c
zouhairmra <zouhairmrabet@gmail.com>
2025-07-18 09:22:52 +0300

    Create 4_Contact.py

---
+++ pages/4_Contact.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("📬 Contact")
+
+st.markdown("""
+For feedback or inquiries, please contact:
+
+📧 **Email:** info@datastatpro.com  
+🌐 **Website:** [www.datastatpro.com](https://www.datastatpro.com)
+""")
